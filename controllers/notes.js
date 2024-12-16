a2120c175602eb9b4481a639b21df136efbffd41
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-12-16 16:09:26 +0200

    Create notes.js

---
+++ controllers/notes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Note } = require('../models');
+
+// Middleware to find a note by ID
+const noteFinder = async (req, res, next) => {
+  req.note = await Note.findByPk(req.params.id);
+  next();
+};
+
+router.get('/', async (req, res) => {
+  const notes = await Note.findAll();
+  res.json(notes);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    res.json(note);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    res.json(req.note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    req.note.important = req.body.important;
+    await req.note.save();
+    res.json(req.note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
