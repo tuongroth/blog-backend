f92c7cec864f6607438a0fbbef18441209f48fe6
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-12-17 19:29:54 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,27 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const { sequelize } = require('../util/db');
+const Blog = require('./blog'); // Import Blog model
+const ReadingList = require('./readingList');
 
 class User extends Model {}
+
 User.init(
   {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    passwordHash: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   },
-  { sequelize, modelName: 'user', underscored: true }
+  {
+    sequelize,
+    modelName: 'User',
+  }
 );
+
+// User can have many blogs through ReadingList
+User.belongsToMany(Blog, {
+  through: ReadingList,
+  foreignKey: 'user_id',
+});
 
 module.exports = User;
