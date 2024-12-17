127b77c4fd6172d65f17102538e03c5587e01e36
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-12-17 15:47:54 +0200

    Update blogs.js

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,31 +1,62 @@
 const router = require('express').Router();
 const { Blog, User } = require('../models');
-const { authenticateUser } = require('../middleware/auth'); // Token validation middleware
+const { Op } = require('sequelize');
+const authenticateUser = require('../middleware/auth');
 
-// GET all blogs with user details
 router.get('/', async (req, res) => {
+  const { search } = req.query;
+  const where = {};
+
+  if (search) {
+    where[Op.or] = [
+      { title: { [Op.iLike]: `%${search}%` } },
+      { author: { [Op.iLike]: `%${search}%` } },
+    ];
+  }
+
   const blogs = await Blog.findAll({
+    where,
     include: {
       model: User,
       as: 'user',
-      attributes: ['id', 'username', 'name'], // Include user info
+      attributes: ['id', 'username', 'name'],
     },
+    order: [['likes', 'DESC']],
   });
+
   res.json(blogs);
 });
 
-// DELETE a blog (only by creator)
-router.delete('/:id', authenticateUser, async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-
-  if (!blog) {
-    return res.status(404).json({ error: 'Blog not found' });
+router.post('/', authenticateUser, async (req, res, next) => {
+  try {
+    const { title, author, content, likes } = req.body;
+    const blog = await Blog.create({
+      title,
+      author,
+      content,
+      likes,
+      userId: req.user.id,
+    });
+    res.status(201).json(blog);
+  } catch (err) {
+    next(err);
   }
-
-  if (blog.userId !== req.user.id) { // Check ownership
-    return res.status(403).json({ error: 'You do not have permission to delete this blog' });
-  }
-
-  await blog.destroy();
-  res.status(204).end();
 });
+
+router.delete('/:id', authenticateUser, async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    if (blog.userId !== req.user.id) {
+      return res.status(403).json({ error: 'Unauthorized to delete this blog' });
+    }
+    await blog.destroy();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
