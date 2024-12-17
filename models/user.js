23595deb44cc6ce340eb389262a9160fbadd9a14
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-12-17 15:48:32 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,32 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 const { sequelize } = require('../util/db');
 
 class User extends Model {}
-
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: 'Name must not be empty' }
-    }
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: { msg: 'Username must not be empty' }
-    }
-  }
-}, {
-  sequelize,
-  modelName: 'user',
-  underscored: true
-});
+  { sequelize, modelName: 'user', underscored: true }
+);
 
 module.exports = User;
